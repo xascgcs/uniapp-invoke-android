06a2ea33a9e9c54375f7b36b70bbf6dba458cc36
xascgcs <xascgcs@qq.com>
2022-03-05 21:41:50 +0800

    feat: 增加原生调用

---
+++ HBuilder-HelloUniApp/app/src/main/java/io/dcloud/HelloH5/wxapi/AddCount.java
@@ -0,0 +1,10 @@
+package io.dcloud.HelloH5.wxapi;
+
+public class AddCount {
+
+    public static int add(int a, int b){
+        System.out.println("----------------------");
+        return  a + b;
+
+    }
+}
